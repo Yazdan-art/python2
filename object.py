30e8a332ad74f5d95c36612e8608bc1bfae74563
Yazdan <yazdan.ghezelbash@gmail.com>
2023-12-04 15:04:34 +0330

    the first act

---
+++ object.py
@@ -0,0 +1,62 @@
+
+# print((lambda a,b:a+b)(20,30))#my idea
+# x=lambda a,b:a+b
+# print(x(20,30))
+# print("\n")
+
+# y=lambda a,b,c:a+b*c
+# print(y(10,20,30))
+#____________________________
+# def myFun(n):
+#     return lambda m:m*n
+# my_value=myFun(2)
+# print(my_value(11))
+#________________________
+# def myFun2(n):
+#     return lambda m:m*n
+# myValue1=myFun2(2)
+# myValue2=myFun2(3)
+# print("my Vlue1:",myValue1(10))
+# print("my Vlue2:",myValue2(20))
+#_________________________
+# class MyClass:
+#     x=5
+    #print("tst my class",x)#myIdea
+#___________________________________    
+# my=MyClass()
+# print(my.x)
+#_________________________________________
+# class Person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def __str__(self):
+#         return f"{self.name}({self.age})"    
+#_________________________________________
+# person=Person("Mohsen",21)
+# print(person.name)
+# print(person.age)
+# print(person)
+#_________________________________________
+#_________________________________________
+#_________________________________________
+class Consider:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+      
+          
+
+    def myFun(self):
+        if(self.age>18):
+          print(self.name+"is more than",self.age)
+
+con=Consider("yazdan",10) 
+con.age=20 
+#del con.age
+con.myFun()
+
+
+#_________________________________________
+#_________________________________________
+#_________________________________________ 
